c1a6cda65dd6e97cb69353822c5c3b69885b4607
Finn Terdal <fterdal@gmail.com>
2017-10-20 14:11:45 -0500

    Setting up the type defs for a blog and install node-fetch

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+const tumblr = require('tumblr.js');
 
 const schema = require('./graphql/schema');
 const PORT = process.env.PORT || 8080;
@@ -14,6 +15,12 @@ const createApp = () => {
   // Connects to Tumblr API
   try {
     require('../secrets');
+    const client = tumblr.createClient({
+      consumer_key: process.env.TUMBLR_COMSUMER_KEY,
+      consumer_secret: process.env.TUMBLR_COMSUMER_SECRET,
+      token: process.env.TUMBLR_TOKEN,
+      token_secret: process.env.TUMBLR_TOKEN_SECRET,
+    });
   } catch(err) {
     console.error(`Please include a secrets.js file at the top level of the project directory with the Tumblr consumer key and secret`);
   }
