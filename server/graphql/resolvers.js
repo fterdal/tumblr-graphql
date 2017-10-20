569f1839a07a2377c488e29222777142e11ba45e
Finn Terdal <fterdal@gmail.com>
2017-10-20 14:15:17 -0500

    Set up skeleton of resolver map

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,1 +1,14 @@
 const fetch = require('node-fetch');
+
+const resolvers = {
+  Query: {
+    blogByIdentifier(_, args) {
+      return {
+        title: 'This is a title',
+        posts: 3,
+      }
+    }
+  }
+};
+
+module.exports = resolvers;
