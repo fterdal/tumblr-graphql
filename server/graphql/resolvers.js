34ccf580fd9bcaf2b6a7ddc69b1b9a32813d74c4
Finn Terdal <fterdal@gmail.com>
2017-10-20 14:52:13 -0500

    Adds a resolver for the Blog type (it seems to work)

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,12 +1,34 @@
-const fetch = require('node-fetch');
+/* eslint camelcase:0 */
+const tumblr = require('tumblr.js');
+
+let client;
+try {
+  require('../../secrets');
+  client = tumblr.createClient({
+    credentials: {
+      consumer_key: process.env.TUMBLR_COMSUMER_KEY,
+      consumer_secret: process.env.TUMBLR_COMSUMER_SECRET,
+      token: process.env.TUMBLR_TOKEN,
+      token_secret: process.env.TUMBLR_TOKEN_SECRET,
+    },
+    returnPromises: true,
+  });
+} catch(err) {
+  console.error(`Please include a secrets.js file at the top level of the project directory with the Tumblr consumer key and secret`);
+}
 
 const resolvers = {
   Query: {
-    blogByIdentifier(_, args) {
-      return {
-        title: 'This is a title',
-        posts: 3,
-      }
+    blogByName(_, {name}) {
+      return client.blogInfo(name, (err, data) => {
+        if (err) throw new Error(err);
+        return data.blog;
+      })
+      .then(data => {
+        // console.log('data', data);
+        return data.blog;
+      })
+      .catch(error => console.error(error));
     }
   }
 };
--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
-const tumblr = require('tumblr.js');
 
 const schema = require('./graphql/schema');
 const PORT = process.env.PORT || 8080;
@@ -11,19 +10,6 @@ const app = express();
 module.exports = app;
 
 const createApp = () => {
-
-  // Connects to Tumblr API
-  try {
-    require('../secrets');
-    const client = tumblr.createClient({
-      consumer_key: process.env.TUMBLR_COMSUMER_KEY,
-      consumer_secret: process.env.TUMBLR_COMSUMER_SECRET,
-      token: process.env.TUMBLR_TOKEN,
-      token_secret: process.env.TUMBLR_TOKEN_SECRET,
-    });
-  } catch(err) {
-    console.error(`Please include a secrets.js file at the top level of the project directory with the Tumblr consumer key and secret`);
-  }
 
   // logging middleware
   app.use(morgan('dev'));
